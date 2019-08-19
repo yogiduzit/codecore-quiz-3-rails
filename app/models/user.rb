6fe3de255d5e5991a0372c83bda5bccbd35222d3
yogiduzit <yverma2@my.bcit.ca>
2019-08-19 10:59:14 -0700

    Added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password 
 
+  has_many :posts, dependent: :destroy
+
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :first_name, presence: true
