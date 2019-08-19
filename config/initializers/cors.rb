1e4d23e21609053dff53b18ed2c9534ae6476b8f
yogiduzit <yverma2@my.bcit.ca>
2019-08-19 13:41:27 -0700

    Can use cookies and configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,13 +4,14 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+# Rails.application.config.action_controller.forgery_protection_origin_check = false
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ['example.com', 'localhost:8080']
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
