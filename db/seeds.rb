aa266317cb4f61c352c8a74bef4da5cfe4067723
yogiduzit <yverma2@my.bcit.ca>
2019-08-19 10:58:25 -0700

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NUM_POSTS = 50
+NUM_USERS = 7
+
+counter = 0
+
+User.create(
+  first_name: "Yogesh",
+  last_name: "Verma",
+  email: "itsyog35h@gmail.com",
+  password: "yogi"
+)
+
+NUM_USERS.times do 
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name}-#{last_name}-#{counter}@gmail.com",
+    password: "supersecret"
+  )
+  counter += 1
+end
+
+NUM_POSTS.times do 
+  Post.create(
+    title: Faker::Educator.subject,
+    body: Faker::ChuckNorris.fact,
+    user: User.all.sample
+  )
+end
