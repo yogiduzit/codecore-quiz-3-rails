3dc77cc858c1f9cdd59c3e122c8f5128fcb42f75
yogiduzit <yverma2@my.bcit.ca>
2019-08-19 10:45:37 -0700

    Set up model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password 
+
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX 
+end
